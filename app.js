8af434daca3a90dbebbd9d12c373230a8820f44d
Lekomtsev Denis <dlekomtsev@ics.perm.ru>
2019-10-08 12:42:47 +0500

    Добавлено редактирование счета, правки багов

--- app.js
+++ app.js
@@ -29,22 +29,24 @@ let App = (function() {
             success: function (data) {
                 $table_data.empty();
                 $.each(data, function(i, v) {
-                    let dateCreated = new Date(v.date_created);
-                    let dateDue = new Date(v.date_due);
-                    let dateSupply = new Date(v.date_supply);
+                    let dateCreated = v.date_created != '' ? new Date(v.date_created) : null;
+                    let dateDue = v.date_due != '' ? new Date(v.date_due) : null;
+                    let dateSupply = v.date_supply != '' ? new Date(v.date_supply) : null;
                     let $tr = $(`<tr>
                                     <td class="hide">${v.id}</td>
                                     <td>${v.number}</td>
                                     <td>${v.direction}</td>
-                                    <td>${dateCreated.toLocaleDateString("ru-RU")}</td>
-                                    <td>${dateDue.toLocaleDateString("ru-RU")}</td>
-                                    <td>${dateSupply.toLocaleDateString("ru-RU")}</td>
+                                    <td>${dateCreated != null ? dateCreated.toLocaleDateString("ru-RU") : ''}</td>
+                                    <td>${dateDue != null ? dateDue.toLocaleDateString("ru-RU") : ''}</td>
+                                    <td>${dateSupply != null ? dateSupply.toLocaleDateString("ru-RU") : ''}</td>
                                     <td>${v.comment}</td>
-                                    <td><button class="btn btn-primary" data-id="${v.id}">Редактировать</button></td>
-                                    <td><button class="btn btn-danger" data-id="${v.id}">Удалить</button></td>
+                                    <td><button type="button" class="btn btn-primary edit-invoice-btn" data-id="${v.id}">Редактировать</button></td>
+                                    <td><button type="button" class="btn btn-danger delete-invoice-btn" data-id="${v.id}">Удалить</button></td>
                                 </tr>`);
                     $table_data.append($tr);
-                });               
+                });
+
+                $('.edit-invoice-btn').on('click', EditInvoiceBtnClickHandler);
             },
             error: function (error) {
                 console.error(error);
@@ -65,6 +67,46 @@ let App = (function() {
         return `f${(+new Date).toString(16)}`;
     }
 
+    let EditInvoiceBtnClickHandler = function() {
+        ClearForm();
+        let $modal = $('#edit-modal');
+        let $form = $modal.find('form');
+        $form.data('id', $(this).data('id'));
+
+        $.ajax({
+            async: true,
+            url: `http://localhost:3000/invoices/${$(this).data('id')}`,
+            method: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                $form.data('date_created', data.date_created);
+                $form.find('#number').val(data.number);
+                $form.find('#direction').val(data.direction);
+                $form.find('#date-due').val(data.date_due.substring(0, data.date_due.indexOf('T')));
+                if (data.date_supply != '')
+                    $form.find('#date-supply').val(data.date_supply.substring(0, data.date_supply.indexOf('T')));
+                $form.find('#comment').val(data.comment);
+                $form.validator();
+
+                $modal.find('.modal-title').text('Редактирование счета');
+                $modal.modal('show');
+            },
+            error: function (error) {
+                console.error(error);
+            }
+        });
+    }
+
+    let ClearForm = function() {
+        let $form = $('#edit-modal form');
+        $form.data('id', '');
+        $form.data('date_created', '');
+        $form.find('#number').val(null);
+        $form.find('#direction').val(null);
+        $form.find('#date-due').val(null);
+        $form.find('#date-supply').val(null);
+        $form.find('#comment').val(null);
+    }
 
     let Init = function() {
         LoadInvoices();
@@ -75,11 +117,12 @@ let App = (function() {
     });
 
     $('#add-invoice-btn').on('click', function() {
+        ClearForm();
         let $modal = $('#edit-modal');
         let $form = $modal.find('form');
         $form.data('id', '');
         $form.validator();
-
+        
         $modal.find('.modal-title').text('Добавление счета');
         $modal.modal('show');
     });
@@ -91,14 +134,14 @@ let App = (function() {
     $('#edit-modal form').validator().on('submit', function(e) {
         if (!e.isDefaultPrevented()) {
             e.preventDefault();
-
             let $form = $(this);
+            let dateSupply = $form.find('#date-supply').val();
             var invoice = {
                 number: parseInt($form.find('#number').val()),
                 direction: $form.find('#direction').val(),
-                date_created: new Date().toLocaleDateString(),
-                date_due: $form.find('#date-due').val(),
-                date_supply: $form.find('#date-supply').val(),
+                date_created: new Date().toISOString(),
+                date_due: new Date($form.find('#date-due').val()).toISOString(),
+                date_supply: dateSupply != '' ? new Date(dateSupply).toISOString() : '',
                 comment: $form.find('#comment').val()
             };
 
@@ -117,6 +160,7 @@ let App = (function() {
             }
             else {
                 invoice.id = $form.data('id');
+                invoice.date_created = $form.data('date_created');
                 $.ajax({
                     async: true,
                     url: `http://localhost:3000/invoices/${invoice.id}`,
